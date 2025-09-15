db7a0d5e4be0179af0e45867c3f0114eda7df8a0
zishanchen <zishanc624@gmail.com>
2025-09-15 14:46:50 +0200

    Time: 7 ms (36.37%), Space: 18.9 MB (16.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^num
+        return missing
